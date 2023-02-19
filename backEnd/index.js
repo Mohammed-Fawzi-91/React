4670222ab24740198d036ddc2c7be783aabb30e3
mohammed fawzi mohammed <mohammed.mohammed.fawzi@gmail.com>
2023-02-19 10:37:50 +0100

    df

--- backEnd/index.js
+++ backEnd/index.js
@@ -11,11 +11,11 @@ const bodyParser = require("body-parser");
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Handle any other requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.js"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.use(cors());
