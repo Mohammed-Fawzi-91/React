fde59a98a4f577463a1f77a2881078574573c133
mohammed fawzi mohammed <mohammed.mohammed.fawzi@gmail.com>
2023-02-27 10:45:23 +0100

    dd

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter as Router } from "react-router-dom";
 
+ReactDOM.render(
+  <Router basename="https://main--animated-jelly-77352e.netlify.app/">
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
